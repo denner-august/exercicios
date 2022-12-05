c85c2a0e5c8d64f62a192f5da79b4ba2a193bfe3
denner augusto <denner.augusto90@gmail.com>
2022-12-05 20:58:29 -0300

    Create exercicio_1010.js

---
+++ uri/exercicio_1010.js
@@ -0,0 +1,14 @@
+const input = require("fs").readFileSync("stdin", "utf8");
+
+let [client1, client2] = input.split("\n");
+
+let [cod1, peca1, value1] = client1.split(" ");
+let [cod2, peca2, value2] = client2.split(" ");
+
+let calcCliente1 = value1 * peca1;
+
+let calcCliente2 = value2 * peca2;
+
+let ValorPagar = calcCliente1 + calcCliente2;
+
+console.log(`VALOR A PAGAR: R$ ${ValorPagar.toFixed(2)}`);
