f3bf16a47fc70a9c652ca31d489f3c2d4d3fdc28
denner augusto <denner.augusto90@gmail.com>
2022-12-12 17:06:31 -0300

    Create exercicio_1017.js

---
+++ uri/exercicio_1017.js
@@ -0,0 +1,9 @@
+const input = require("fs").readFileSync("stdin", "utf8");
+
+let [tempo, velocidade] = input.split("\n");
+
+let litros = 12;
+
+let gasolina = (velocidade / litros) * tempo;
+
+console.log(gasolina.toFixed(3));
