5f3bcdaab4d1cc3432c5b59bb332878ec1378899
denner augusto <denner.augusto90@gmail.com>
2022-12-14 16:15:42 -0300

    atualizando exercicio

---
+++ uri/exericico_1018.js
@@ -0,0 +1,14 @@
+console.clear();
+const input = require("fs").readFileSync("stdin", "utf8");
+
+let [numero] = input.split("\n");
+
+let notas = [100];
+
+let atual = 0;
+
+notas.reduce((preve, next, index, array) => {
+  let teste = [];
+
+  return preve;
+}, 0);
