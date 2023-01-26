5b9c1c03b1dfad43dbe2c170827615cf88e591df
denner augusto <denner.augusto90@gmail.com>
2023-01-26 19:55:35 -0300

    feat(project): adicionando novos desafios

--- uri/exercicio_1018.js
+++ uri/exercicio_1018.js
@@ -3,64 +3,33 @@ const input = require("fs").readFileSync("stdin", "utf8");
 
 let [total] = input.split("\n");
 
+
+let decomposicao = {};
+
 let numeroTotal = total;
 
 let notas = [100, 50, 20, 10, 5, 2, 1];
 
-let decomposicao = {};
+notas.forEach((nota) => {
+  decomposicao[nota] = 0;
+  while (numeroTotal >= nota) {
+    decomposicao[nota]++;
+    numeroTotal -= nota;
+  };
+});
 
+const result = notas.map((nota) => {
+  return `${decomposicao[nota]} nota(s) de ${nota.toLocaleString("pt-br", {
+    style: "currency",
+    currency: "BRL",
+  })}`;
+});
 
- function teste() {
-    let caixa = []
-  
-  notas.forEach((nota) => {
-    decomposicao[nota] = 0;
-    while (numeroTotal >= nota) {
-      decomposicao[nota]++;
-      numeroTotal -= nota;
-    }
-  });
-  
-  const result = notas.map((nota) => {
-    return `${decomposicao[nota]} nota(s) de ${nota.toLocaleString("pt-br", {
-      style: "currency",
-      currency: "BRL",
-    })}`;
-  });
-  
-  
-  result.unshift(total);
-  
-  
-   result.reduce((preve, next, index, arr) => {
-    preve = next;
-  
-    caixa.push(preve)
-    
-    return preve;
-  }, []);
+result.unshift(total);
 
-  return caixa
-}
-
-let tes = teste()
-
-console.log(tes.join("\n"));
+result.forEach(item => console.log(item))
 
 
 
-// console.log(caixa.join("\n"));
 
-/*
 
-// const result = notas.map((nota) => {
-//   return `${decomposicao[nota]} nota(s) de ${nota.toLocaleString("pt-br", {
-//     style: "currency",
-//     currency: "BRL",
-//   })}`;
-// });
-
-*/
-  // const result = notas.map((nota) => {
-  //   return `${decomposicao[nota]} nota(s) de ${nota.toFixed(2)}`;
-  // });
