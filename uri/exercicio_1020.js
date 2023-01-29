c77a1483ecfad77298d5c7a4e7e0b6883623cf64
denner augusto <denner.augusto90@gmail.com>
2023-01-28 22:35:32 -0300

    Update exercicio_1020.js

--- uri/exercicio_1020.js
+++ uri/exercicio_1020.js
@@ -3,38 +3,31 @@ const input = require("fs").readFileSync("stdin", "utf8");
 
 let [number] = input.split("\n");
 
-let segundos = number
+let segundos = parseInt(number)
 
 let numeros = [365, 30]
 
 let datas = {}
 
-numeros.forEach(number => {
 
-    let calc = Math.round(segundos / number)
+for (let index = 0; index < numeros.length; index++) {
 
-    let multiplica = (number * calc)
+    let calc = Math.round(segundos / numeros[index])
+
+    let multiplica = (numeros[index] * calc)
 
     segundos = segundos - multiplica
 
-    // datas[`${number}`] = calc
+    if (numeros[index] === 365) {
+        datas.ano = calc
 
-    switch (number) {
-
-        case number == 365:
-            datas.ano = calc
-            break;
-
-        case number == 30:
-            datas.mes = calc
-            break;
-
-        default:
-            datas.dias = segundos
-            break;
     }
 
-});
+    if (numeros[index] === 30) {
+        datas.mes = calc
+    }
 
+    datas.dias = segundos
+}
 
-console.log(datas);
+console.log(`${datas.ano} ano(s)\n${datas.mes} mes(es)\n${datas.dias} dia(s)\n`);
