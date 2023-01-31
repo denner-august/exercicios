b6f8289bdfc26e27a6170d70ba6c0ec8c16f8e9d
denner augusto <denner.augusto90@gmail.com>
2023-01-31 12:04:53 -0300

    exercicio de numero 1019 do beecrowd

--- uri/exercicio_1019.js
+++ uri/exercicio_1019.js
@@ -3,21 +3,11 @@ const input = require("fs").readFileSync("stdin", "utf8");
 
 let [numero] = input.split(" ");
 
-function conversor(numero) {
-    
-    let horas = numero / 3600;
-    let minutos = new Date(numero * 1000).getUTCMinutes();
-    let segundos = new Date(numero * 1000).getSeconds();
+let horas = numero / 3600;
+let minutos = new Date(numero * 1000).getUTCMinutes();
+let segundos = new Date(numero * 1000).getSeconds();
 
-    let teste = Math.round(horas) <= 0 ? 0 : Math.round(horas - 1);
-    let teste2 = Math.round(minutos)
-
-    console.log(`${Math.ceil(horas)}:${teste2}:${segundos}`);
-}
-
-
-
-conversor(251264)
+console.log(`${Math.floor(horas)}:${Math.round(minutos)}:${segundos}`);
 
 
 
@@ -25,4 +15,7 @@ conversor(251264)
 
 
 
-  
+
+
+
+
